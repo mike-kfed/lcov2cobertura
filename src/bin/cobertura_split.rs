2e28c4e99e2b2ab070a04a85707279b87d61fe1f
Michael Kefeder <m.kefeder@gmail.com>
2022-11-20 13:00:44 +0100

    issue #4 adding helper tool to split large cobertura files

---
+++ src/bin/cobertura_split.rs
@@ -0,0 +1,21 @@
+//! executable to efficiently split huge cobertura XML files
+use std::path::PathBuf;
+
+use clap::Parser;
+
+use lcov2cobertura as lcov2xml;
+
+/// Cmd line arguments
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// cobertura XML input file
+    #[clap()]
+    filename: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    lcov2xml::corbertura_xml_split(&args.filename)?;
+    Ok(())
+}
